7158c7a131d1b4816bfb6fb9b0a19eba0c7c564a
wb_wangxiaoguang02 <wb_wangxiaoguang02@meituan.com>
2024-05-09 13:21:51 +0800

    2

---
+++ miniProgram/api/user.js
@@ -0,0 +1,8 @@
+// 导入封装的网络请求模块实例
+import http from '../utils/http'
+/**
+ * @description 进行登录操作
+ * @param {*} code 获取到的临时登录凭证
+ * @returns Promise
+ */
+export const reqLogin = (code) => http.get(`/weixin/wxLogin/${code}`)
--- miniProgram/pages/login/login.js
+++ miniProgram/pages/login/login.js
@@ -1,2 +1,39 @@
 // pages/login/login.js
-Page({})
+// 导入封装通用模块方法
+import { toast } from '../../utils/extendApi'
+// 导入接口 API 函数
+import { reqLogin } from '../../api/user'
+// 导入本地存储 api
+import { setStorage } from '../../utils/storage'
+Page({
+  login() {
+    wx.login({
+      success: async ({ code }) => {
+        if (code) {
+          // 在获取到临时登录凭证 code 以后,需要传递给开发者服务器
+          const res = await reqLogin(code)
+          console.log(res)
+          const { token } = res.data
+          const ress = setStorage('token', token)
+          console.log(ress)
+        } else {
+          toast({ title: '授权失败,请重新登录' })
+        }
+      }
+    })
+
+    // wx.login({
+    //   // success: (res) => {
+    //   // 直接把code从res里面解构出来
+    //   success: ({ code }) => {
+    //     // console.log(res)
+    //     if (code) {
+    //       // 在获取到临时登录凭证 code 以后,需要传递给开发者服务器
+    //       console.log(code)
+    //     } else {
+    //       toast({ title: '授权失败,请重新登录' })
+    //     }
+    //   }
+    // })
+  }
+})
